e06bee7ada5e897012e83cf7b6f036df24dbe3ff
Sulis Afryanti <145976767+SULISAFRYANTI@users.noreply.github.com>
2024-10-04 12:44:57 +0700

    Add files via upload

---
+++ mvc/views/home/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/styleIndexUtama.css">
+</head>
+<body>
+<h1>Selamat Datang di Aplikasi Penjualan Barang</h1>
+<p>Ini adalah halaman home.</p>
+<a href="<?= BASEURL; ?>index.php?url=login/index">Login</a>
+</body>
+</html>
+
+
---
+++ mvc/views/login/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="assets/styleindexadmin.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Form Login</h2>
+        <form action="<?= BASEURL; ?>index.php?url=login/auth" method="POST">
+            <input type="text" name="username" placeholder="Username" required><br>
+            <input type="password" name="password" placeholder="Password" required><br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>
